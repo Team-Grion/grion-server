4b45ce626accf571c04e27a132680b0e309dcdd7
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-06-02 00:57:56 +0900

    [mod/#5] 모델명 수정

--- src/main/kotlin/com/project/grionserver/domain/moderation/dto/ModerationRequestDto.kt
+++ src/main/kotlin/com/project/grionserver/domain/moderation/dto/ModerationRequestDto.kt
@@ -2,5 +2,5 @@ package com.project.grionserver.domain.moderation.dto
 
 data class ModerationRequestDto(
     val input: String,
-    val model: String = "text-moderation-latest"
+    val model: String = "omni-moderation-latest"
 )

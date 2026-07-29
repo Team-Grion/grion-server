4ad8523751e349beb7e0b75e3c9f3d45d3ad5697
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-29 19:35:07 +0900

    [mod/#8] PetController 수정

--- src/main/kotlin/com/project/grionserver/domain/pet/controller/PetController.kt
+++ src/main/kotlin/com/project/grionserver/domain/pet/controller/PetController.kt
@@ -3,12 +3,11 @@ package com.project.grionserver.domain.pet.controller
 import com.project.grionserver.domain.pet.dto.PetCreateRequest
 import com.project.grionserver.domain.pet.dto.PetCreateResponse
 import com.project.grionserver.domain.pet.service.PetService
-import org.springframework.format.annotation.DateTimeFormat
+import com.project.grionserver.global.response.ApiResponse
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
-import java.time.LocalDate
 
 @RestController
 @RequestMapping("/pet")
@@ -17,29 +16,20 @@ class PetController(private val petService: PetService) {
     @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun createPet(
         @RequestParam userId: Long,
-        @RequestParam name: String,
-        @RequestPart image: MultipartFile,
+        @RequestPart("petImageUrl") petImage: MultipartFile,
         @RequestParam species: String,
         @RequestParam breed: String,
-        @RequestParam(required = false)
-        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) birthday: LocalDate?,
-        @RequestParam
-        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) deathDate: LocalDate?,
-        @RequestParam characteristics: String?,
-        @RequestParam(required = false) memories: String?,
-        @RequestParam backgroundText: String?
-    ): ResponseEntity<PetCreateResponse> {
+        @RequestParam personalities: List<String>,
+        @RequestParam background: String
+    ): ResponseEntity<ApiResponse<PetCreateResponse>> {
         val request = PetCreateRequest(
             userId = userId,
-            name = name,
             species = species,
             breed = breed,
-            birthday = birthday,
-            deathDate = deathDate,
-            characteristics = characteristics,
-            memories = memories,
-            backgroundText = backgroundText
+            personalities = personalities,
+            background = background
         )
-        return ResponseEntity.ok(petService.createPet(image, request))
+        val response = petService.createPet(petImage, request)
+        return ResponseEntity.ok(ApiResponse.success(response))
     }
 }

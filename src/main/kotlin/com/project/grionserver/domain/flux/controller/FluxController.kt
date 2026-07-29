9245a10475a16b1777871fea9c08fac685173e88
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-07-29 18:32:28 +0900

    [feat/#10] 내부 api operation 추가

--- src/main/kotlin/com/project/grionserver/domain/flux/controller/FluxController.kt
+++ src/main/kotlin/com/project/grionserver/domain/flux/controller/FluxController.kt
@@ -2,15 +2,21 @@ package com.project.grionserver.domain.flux.controller
 
 import com.project.grionserver.domain.flux.dto.FluxEditResponse
 import com.project.grionserver.domain.flux.service.FluxService
+import io.swagger.v3.oas.annotations.Hidden
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 
+@Hidden
+@Tag(name = "Flux", description = "AI 이미지 편집 API")
 @RestController
 @RequestMapping("/flux")
 class FluxController(private val fluxService: FluxService) {
 
+    @Operation(summary = "이미지 편집", description = "반려동물 이미지를 AI로 편집합니다.")
     @PostMapping("/edit", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun editImage(
         @RequestPart("image") image: MultipartFile,
--- src/main/kotlin/com/project/grionserver/domain/translate/controller/TranslatorController.kt
+++ src/main/kotlin/com/project/grionserver/domain/translate/controller/TranslatorController.kt
@@ -2,17 +2,23 @@ package com.project.grionserver.domain.translate.controller
 
 import com.project.grionserver.domain.translate.dto.internal.TranslationRequest
 import com.project.grionserver.domain.translate.service.TranslatorService
+import io.swagger.v3.oas.annotations.Hidden
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
+@Hidden
+@Tag(name = "Translation", description = "번역 API")
 @RestController
 @RequestMapping("/api/internal/translation")
 class TranslationController(
     private val translatorService: TranslatorService,
 ) {
+    @Operation(summary = "프롬프트 번역", description = "AI 프롬프트를 영어로 번역합니다.")
     @PostMapping("/translate")
     fun translatePrompt(
         @RequestBody request: TranslationRequest

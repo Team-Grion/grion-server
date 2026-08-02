552c85e14ba6acfa395a8506289fb2aeb099cd91
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-08-02 19:41:02 +0900

    [feat/#16] 메세지 개수 count 함수 추가

--- src/main/kotlin/com/project/grionserver/domain/message/repository/MessageRepository.kt
+++ src/main/kotlin/com/project/grionserver/domain/message/repository/MessageRepository.kt
@@ -3,7 +3,9 @@ package com.project.grionserver.domain.message.repository
 import com.project.grionserver.domain.message.entity.Message
 import com.project.grionserver.domain.pet.entity.Pet
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 
 @Repository
 interface MessageRepository : JpaRepository<Message, Long> {
@@ -12,4 +14,11 @@ interface MessageRepository : JpaRepository<Message, Long> {
 
     // 특정 반려동물에게 온 메시지 개수
     fun countByPet(pet: Pet): Long
+
+    // 특정 반려동물에게 오늘 온 메시지 개수
+    fun countByPetAndCreatedAtBetween(pet: Pet, start: LocalDateTime, end: LocalDateTime): Long
+
+    // 공개된 추모 공간에 오늘 온 메시지 개수
+    @Query("SELECT COUNT(m) FROM Message m WHERE m.pet.isShared = true AND m.createdAt BETWEEN :start AND :end")
+    fun countTodayMessagesForPublicMemorials(start: LocalDateTime, end: LocalDateTime): Long
 }

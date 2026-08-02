5c8f161813fa212ad4ce93f6476256a9cd30f893
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-08-02 19:41:38 +0900

    [feat/#16] 추모 공간 개수 count 함수 추가

--- src/main/kotlin/com/project/grionserver/domain/pet/repository/PetRepository.kt
+++ src/main/kotlin/com/project/grionserver/domain/pet/repository/PetRepository.kt
@@ -1,9 +1,11 @@
 package com.project.grionserver.domain.pet.repository
 
 import com.project.grionserver.domain.pet.entity.Pet
+import com.project.grionserver.domain.pet.entity.Species
 import com.project.grionserver.domain.user.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 
 @Repository
 interface PetRepository : JpaRepository<Pet, Long> {
@@ -12,4 +14,10 @@ interface PetRepository : JpaRepository<Pet, Long> {
 
     // 공개된 추모 페이지 목록 조회
     fun findAllByIsSharedTrue(): List<Pet>
+
+    // 공개된 추모 페이지 목록 조회
+    fun findAllByIsSharedTrueAndSpecies(species: Species): List<Pet>
+
+    // 오늘 새로 등록된 공개 추모 공간 개수
+    fun countByIsSharedTrueAndCreatedAtBetween(start: LocalDateTime, end: LocalDateTime): Long
 }

9593fa8fab06db29823d7083fbb2d419a3a5155e
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-08-03 20:23:26 +0900

    [mod/#25] 원본 exception 전달

--- src/main/kotlin/com/project/grionserver/global/exception/UnauthorizedException.kt
+++ src/main/kotlin/com/project/grionserver/global/exception/UnauthorizedException.kt
@@ -1,3 +1,3 @@
 package com.project.grionserver.global.exception
 
-class UnauthorizedException(message: String) : RuntimeException(message)
+class UnauthorizedException(message: String, cause: Throwable? = null) : RuntimeException(message, cause)

82beaa93d09fd3cb004bc23f09c23639bc8a9f29
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-08-03 19:59:27 +0900

    [feat/#25] JWT 라이브러리 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,11 @@ dependencies {
     // HTTP
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 }
 
 kotlin {

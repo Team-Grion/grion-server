895d27a3229884e1ada21ab025540ae0c9d57f71
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-08-05 16:25:31 +0900

    [feat/#10] Auth 태그 스웨거 추가

--- src/main/kotlin/com/project/grionserver/domain/auth/controller/AuthController.kt
+++ src/main/kotlin/com/project/grionserver/domain/auth/controller/AuthController.kt
@@ -4,6 +4,8 @@ import com.project.grionserver.domain.auth.dto.KakaoLoginRequest
 import com.project.grionserver.domain.auth.dto.KakaoLoginResponse
 import com.project.grionserver.domain.auth.service.AuthService
 import com.project.grionserver.global.response.ApiResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "Auth", description = "인증 API")
 @RestController
 @RequestMapping("/auth")
 class AuthController(private val authService: AuthService) {
 
+    @Operation(summary = "카카오 로그인", description = "JWT 액세스/리프레시 토큰을 발급합니다.")
     @PostMapping("/kakao")
     fun loginWithKakao(
         @Valid @RequestBody request: KakaoLoginRequest

53093c5ba438c989b1c12330320dd6053e82811d
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-08-05 21:00:01 +0900

    [feat/#10] 스웨거 인증 추가

--- src/main/kotlin/com/project/grionserver/global/config/SwaggerConfig.kt
+++ src/main/kotlin/com/project/grionserver/global/config/SwaggerConfig.kt
@@ -1,7 +1,10 @@
 package com.project.grionserver.global.config
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -9,11 +12,26 @@ import org.springframework.context.annotation.Configuration
 class SwaggerConfig {
 
     @Bean
-    fun openAPI(): OpenAPI = OpenAPI()
-        .info(
-            Info()
-                .title("Grion API")
-                .description("Grion 서버 API 명세서")
-                .version("v1.0.0")
-        )
+    fun openAPI(): OpenAPI {
+        val securitySchemeName = "bearerAuth"
+
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Grion API")
+                    .description("Grion 서버 API 명세서")
+                    .version("v1.0.0")
+            )
+            .addSecurityItem(SecurityRequirement().addList(securitySchemeName))
+            .components(
+                Components().addSecuritySchemes(
+                    securitySchemeName,
+                    SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                )
+            )
+    }
 }

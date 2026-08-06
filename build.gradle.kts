64df88a65e82c65ae54dc3b63ea2f7ac48e6d014
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-08-06 17:30:58 +0900

    [feat/#39] Flyway 도입 및 초기 스키마 마이그레이션 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
